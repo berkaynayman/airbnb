97c0075a8aaa3276fd41df180f94ea5e3171a47c
Berkay Nayman <berkaynayman4@gmail.com>
2023-06-30 15:00:44 +0300

    Auth UI.

--- app/hooks/RegisterModal.tsx
+++ app/hooks/RegisterModal.tsx
@@ -13,6 +13,10 @@ import {
 
 import useRegisterModal from "./useRegisterModal";
 import Modal from "../components/modals/Modal";
+import Heading from "../components/Heading";
+import Input from "../components/inputs/Input";
+import { toast } from "react-hot-toast";
+import Button from "../components/Button";
 
 const RegisterModal = () => {
     const registerModal = useRegisterModal();
@@ -39,13 +43,89 @@ const RegisterModal = () => {
             .then(() => {
                 registerModal.onClose();
             })
-            .catch((error) => {
-                console.log(error);
+            .catch((error) => { 
+                toast.error('Something went wrong!')
             })
             .finally(() => {
                 setIsLoading(false);
             })
     }
+
+    const bodyContent = (
+        <div className="flex flex-col gap-4">
+            <Heading
+                title="Welcome to Airbnb"
+                subtitle="Create an account!"
+            />
+            <Input
+                id="email"
+                label="Email"
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+            />
+            <Input
+                id="name"
+                label="Name"
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+            />
+            <Input
+                id="password"
+                label="Password"
+                type="password"
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+            />
+        </div>
+    )
+
+    const footerContent = (
+        <div className="flex flex-col gap-4 mt-3">
+            <hr />
+            <Button
+                outline
+                label="Continue with Google"
+                icon={FcGoogle}
+                onClick={() => {}}
+            />
+            <Button
+                outline
+                label="Continue with Github"
+                icon={AiFillGithub}
+                onClick={() => {}}
+            />
+            <div
+                className="
+                    text-neutral-500
+                    text-center
+                    mt-4
+                    font-light
+                "
+            >
+                <div className="justify-center flex flex-row items-center gap-2">
+                    <div>
+                        Already have an account?
+                    </div>
+                    <div
+                        onClick={registerModal.onClose}
+                        className="
+                            text-neutral-800
+                            cursor-pointer
+                            hover:underline
+                        "
+                    >
+                        Log in
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 
     return (
         <Modal
@@ -55,6 +135,8 @@ const RegisterModal = () => {
             actionLabel="Continue"
             onClose={registerModal.onClose}
             onSubmit={handleSubmit(onSubmit)}
+            body={bodyContent}
+            footer={footerContent}
         />
     )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import Navbar from './components/navbar/Navbar';
 import Modal from './components/modals/Modal';
 import RegisterModal from './hooks/RegisterModal';
+import ToasterProvider from './providers/ToasterProvider';
 
 export const metadata = {
   title: 'Airbnb - Berkay Nayman',
@@ -22,6 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ToasterProvider />
         <RegisterModal />
         <Navbar />
         {children}

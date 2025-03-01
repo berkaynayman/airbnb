b16ff49b6b3eb794add2e6924af85807dc348ae9
Berkay Nayman <berkaynayman4@gmail.com>
2025-03-01 23:11:03 +0100

    Fixed properties/page typescript error.

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -37,7 +37,7 @@ const PropertiesPage = async () => {
     return (
         <ClientOnly>
             <PropertiesClient
-                listings={listings}
+                reservations={listings}
                 currentUser={currentUser}
             />
         </ClientOnly>
